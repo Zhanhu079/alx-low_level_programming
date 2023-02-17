026d792f75544fb3b959718a3d0d2e7c7ca89ac3
Zhanhu079 <keitumetse1233@gmail.com>
2023-02-17 08:51:02 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	char str1[32] = "abcdfghijklm";
+
+	char str2[] = "noprstuvwxyz";
+
+	strcat(str1, str2);
+	while (str1[i] != '\0')
+	{
+		putchar(str1[i]);
+		i++;
+	}
+	return (0);
+}
