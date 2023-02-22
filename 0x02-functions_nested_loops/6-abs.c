51e0fb97cefd0c21dbc605944ae98376a68e566b
Zhanhu079 <keitumetse1233@gmail.com>
2023-02-22 09:30:20 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - finds the absolute value of the argument
+ * @n: the argument that will be checked
+ *Return: Always num
+ */
+int _abs(int n)
+{
+	int num = abs(n);
+
+	return (num);
+}
